2933cbe6b01ccab37deaea06f45f8b0225b277d0
waxeve45 <sriboonruang2545@gmail.com>
2024-10-23 21:15:54 +0700

    ควยหน้าหีเย็ดแม่

--- src/app/adminwork/adminwork.component.ts
+++ src/app/adminwork/adminwork.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, RouterLink, RouterModule } from '@angular/router';
+import { DataService } from '../../data.service';
+import { User } from '../model/usermodel/user.model';
+import { Work } from '../model/workmodel/work.model';
 
 @Component({
   selector: 'app-adminwork',
   standalone: true,
-  imports: [RouterLink,RouterModule],
+  imports: [RouterLink,RouterModule,CommonModule,HttpClientModule],
   templateUrl: './adminwork.component.html',
   styleUrl: './adminwork.component.scss'
 })
-export class AdminworkComponent {
-
+export class AdminworkComponent implements OnInit {
+editWork(_t32: Work) {
+throw new Error('Method not implemented.');
+}
+deleteWork(arg0: any) {
+throw new Error('Method not implemented.');
+}
+    works : Work[] = [];
+  
+    constructor(
+    private dataService: DataService,
+    private http: HttpClient,
+    private router: Router,
+    private route: ActivatedRoute
+    ){}
+  
+    ngOnInit(){
+      this.getwork();
+    }
+  
+    getwork(){
+      this.http.get(this.dataService.apiEndpoint + '/getWork').subscribe((respone: any) => {
+        this.works = respone;
+      })
+    }
 }
---
+++ src/app/model/workmodel/work.model.ts
@@ -0,0 +1,23 @@
+// To parse this data:
+//
+//   import { Convert } from "./file";
+//
+//   const work = Convert.toWork(json);
+
+export interface Work {
+workCode: any;
+    workName:        string;
+    work_date_Start: string;
+    work_date_End:   string;
+}
+
+// Converts JSON strings to/from your types
+export class Convert {
+    public static toWork(json: string): Work {
+        return JSON.parse(json);
+    }
+
+    public static workToJson(value: Work): string {
+        return JSON.stringify(value);
+    }
+}
